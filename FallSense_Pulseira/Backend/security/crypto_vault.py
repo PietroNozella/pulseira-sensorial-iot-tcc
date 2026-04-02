a91dc311d6fc6a3d376da744dd1e48d74a67032a
Bruno Shiraishi <brunno.shiraishi@outlook.com>
2026-04-02 14:39:49 -0300

    feat(security): implementada criptografia AES-256 e gestao de chaves (Req 3.4, 3.5, 3.6)
    Finalizada a blindagem dos dados sensíveis. O sistema agora utiliza variáveis de ambiente (.env) para proteger a Master Key e criptografa os dados antes do envio ao Supabase.

--- FallSense_Pulseira/Backend/security/crypto_vault.py
+++ FallSense_Pulseira/Backend/security/crypto_vault.py
@@ -1,33 +1,34 @@
-from cryptography.fernet import Fernet
 import os
+from cryptography.fernet import Fernet
 from dotenv import load_dotenv
 
-# caminho para p python achar o .env na pasta do beckend, mesmo estando 
-# na pasta security. 
+# --- CONFIGURAÇÃO DO AMBIENTE (REQ 3.6) ---
+# Aqui eu busco o arquivo .env que tá na raiz do projeto
 base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 dotenv_path = os.path.join(base_dir, ".env")
 load_dotenv(dotenv_path)
 
-# chave do arquivo .env
+# Pego a chave mestra que a gente salvou no .env
 SECRET_KEY = os.getenv("ENCRYPTION_KEY")
 
+# Trava de segurança: se a chave não estiver no .env, o sistema nem abre
 if not SECRET_KEY:
-    # evita rodar sem egurança.
-    raise ValueError("ERRO CRÍTICO: ENCRYPTION_KEY não encontrada no .env!")
+    print("\n[ERRO] Cade a ENCRYPTION_KEY no .env? Arruma aí!")
+    raise SystemExit("Sistema parado por falta de chave de segurança.")
 
-# Preparando o motor Fernet (AES-256)
+# Inicializo o motor de criptografia (AES-256)
 fernet = Fernet(SECRET_KEY.encode())
 
 def proteger_dado(texto_limpo: str) -> str:
-    """Transforma o segredo em código AES-256 para o Supabase"""
+    """ REQ 3.4: Criptografa o dado antes de salvar no banco """
     if not texto_limpo:
         return None
-    # Encripta os bytes e decodifica para string para salvar no banco
+    # Transforma o texto em "sopa de letras" (Ciphertext)
     return fernet.encrypt(texto_limpo.encode()).decode()
 
 def abrir_dado(texto_criptografado: str) -> str:
-    """Transforma o código do Supabase de volta em texto original"""
+    """ REQ 3.5: Abre o dado que veio do banco pra gente ler """
     if not texto_criptografado:
         return None
-    # Descriptografa o código usando a chave mestra
+    # Devolve o texto original usando a chave mestra
     return fernet.decrypt(texto_criptografado.encode()).decode()
